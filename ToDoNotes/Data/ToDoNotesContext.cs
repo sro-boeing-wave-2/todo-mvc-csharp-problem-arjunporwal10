2db18c1e0b17090566a8f610bd3c267c82d6fb00
arjun <arjunporwal10@gmail.com>
2018-08-04 15:24:54 +0530

    added swagger and travis

---
+++ ToDoNotes/Data/ToDoNotesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Notes.Models;
+
+namespace ToDoNotes.Models
+{
+    public class ToDoNotesContext : DbContext
+    {
+        public ToDoNotesContext (DbContextOptions<ToDoNotesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Notes.Models.ToDo> ToDo { get; set; }
+    }
+}
---
+++ ToDoNotes/Migrations/20180804085108_initial.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoNotes.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Text = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    IsPinned = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Checklist",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ChecklistData = table.Column<string>(nullable: true),
+                    IsChecked = table.Column<bool>(nullable: false),
+                    ToDoId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Checklist", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Checklist_ToDo_ToDoId",
+                        column: x => x.ToDoId,
+                        principalTable: "ToDo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Label",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    LabelName = table.Column<string>(nullable: true),
+                    ToDoId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Label", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Label_ToDo_ToDoId",
+                        column: x => x.ToDoId,
+                        principalTable: "ToDo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Checklist_ToDoId",
+                table: "Checklist",
+                column: "ToDoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Label_ToDoId",
+                table: "Label",
+                column: "ToDoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Checklist");
+
+            migrationBuilder.DropTable(
+                name: "Label");
+
+            migrationBuilder.DropTable(
+                name: "ToDo");
+        }
+    }
+}
